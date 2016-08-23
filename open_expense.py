f983656685407f0d11ab1b7c83b590fcbeb468c2
Shutao Zhang <Shutao Zhang>
2016-08-24 00:27:25 +0800

    read expense, in progress

--- open_expense.py
+++ open_expense.py
@@ -7,6 +7,16 @@ from xlrd import open_workbook
 from xlrd.xldate import xldate_as_datetime
 from DIF.utility import logger, get_datemode, retrieve_or_create
 from DIF.open_holding import is_empty_cell
+from DIF.open_summary import read_date, find_cell_string
+
+
+
+class InvalidExpenseItem(Exception):
+	"""
+	An error condition when a row is found to not containing the right
+	cell values for an expense item.
+	"""
+	pass
 
 
 
@@ -20,36 +30,36 @@ def read_expense(ws, port_values):
 		keys: date, description, amount, currency, 
 		exchange_rate, hkd_equivalent
 	"""
-	row = 0
+	row = find_cell_string(ws, 0, 0, 'Valuation Period :')
+	expense_date = read_date(ws, row, 1)
 
-	n, expense_date = read_date(ws, row)
+	n = find_cell_string(ws, 0, 0, 'Value Date')
 	row = row + n
-
-	while row < ws.nrows:
-		cell_value = ws.cell_value(row, 0)
-		
-		row = row + 1	# end of while loop
-	
 	fields = read_expense_fields(ws, row)
-
-
 	expenses = retrieve_or_create(port_values, 'expense')
 
-	while (row < ws.nrows):
-		while (is_blank_line(ws, row)):
-			row = row + 1
+	while (is_blank_line(ws, row, 9)):	# skip blank lines in between
+		row = row + 1
 
+	while (row < ws.nrows):
 		try:
-			read_expense_item(ws, row, expenses)
-		except (ValueError, TypeError):
+			read_expense_item(ws, row, fields, expenses)
+		except InvalidExpenseItem:
 			# this line is not a expense item, skip it
 			pass
 
 		row = row + 1
+		if is_blank_line(ws, row, 9):	# end of the first expense section
+			break
+
 		# end of while loop
 
+	while (is_blank_line(ws, row, 9)):	# skip blank lines in between
+		row = row + 1
 
-	
+	expense_sub_total = read_expense_sub_total(ws, row)
+
+
 
 def is_blank_line(ws, row, n_cells):
 	"""
@@ -71,14 +81,10 @@ def read_expense_fields(ws, row):
 
 	field_mapping = {'Value Date':'value_date', 'Description':'description', 
 						'Amount':'amount', 'CCY':'currency', 'Rate':'fx_rate', 
-						'HKD Equiv.':'hkd_equivalent'}
+						'HKD Equiv.':'hkd_equivalent', '':'empty_field'}
 
 	for column in range(9):	# read up to column I
-		if is_empty_cell(ws, row, column):
-			fld = 'empty_field'
-			fields.append(fld)
-			continue
-
+		
 		cell_value = ws.cell_value(row, column)
 		if not isinstance(cell_value, str):	# data field name needs to
 											# be string
@@ -97,3 +103,67 @@ def read_expense_fields(ws, row):
 
 
 	return fields
+
+
+
+def read_expense_item(ws, row, fields, expenses):
+	"""
+	Read the row and try to create an expense item based on content
+	in the row. If successful, append it to the expenses list,
+	if not, an InvalidExpenseItem exception is thrown.
+
+	If amount of the expense is 0, then no expense item is created.
+	"""
+
+	exp_item = {}	# the expense item
+	column = -1
+	for fld in fields:
+		column = column + 1
+		if fld == 'empty_field':
+			continue
+
+		cell_value = ws.cell_value(row, column)
+
+		if fld in ['value_date', 'amount', 'fx_rate', 'hkd_equivalent']:
+			if not isinstance(cell_value, float):
+				logger.error('read_expense_item(): field {0} is not float, value = {1}'.
+							format(fld, cell_value))
+				raise InvalidExpenseItem('invalid field type')
+
+			if fld == 'value_date':
+				datemode = get_datemode()
+				exp_item[fld] = xldate_as_datetime(cell_value, datemode)
+			elif fld == 'amount' and cell_value == 0:	# stop reading
+				logger.warning('read_expense_item(): amount is 0 at row {0}'.
+								format(row))
+				return
+
+			else:
+				exp_item[fld] = cell_value
+
+		elif fld in ['description', 'currency']:
+			if not isinstance(cell_value, str):
+				logger.error('read_expense_item(): field {0} is not string, value = {1}'.
+							format(fld, cell_value))
+				raise InvalidExpenseItem('invalid field type')
+
+			exp_item[fld] = str.strip(cell_value)
+
+		else:	# unexpected field type
+			logger.error('read_expense_item(): field {0} is unexpected'.format(fld))
+			raise InvalidExpenseItem('unexpected field type')
+
+		# end of for loop
+
+	expenses.append(exp_item)
+
+
+
+def read_expense_sub_total(ws, row):
+	sub_total = ws.cell_value(row, 8)
+	if isinstance(sub_total, float):
+		return sub_total
+	else:
+		logger.error('read_expense_sub_total(): subtotal is not a float, value = {0}'.
+						format(subtotal))
+		raise ValueError('invalid subtotal type')
--- test/test_expense.py
+++ test/test_expense.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 """
 Test methods open_expense.py
 
@@ -7,7 +8,9 @@ import unittest2
 import datetime
 from xlrd import open_workbook
 from DIF.utility import get_current_path
-from DIF.open_summary import read_date
+from DIF.open_summary import read_date, find_cell_string
+from DIF.open_expense import read_expense_fields, read_expense_item, \
+                            InvalidExpenseItem
 
 class TestExpense(unittest2.TestCase):
 
@@ -46,5 +49,82 @@ class TestExpense(unittest2.TestCase):
         filename = get_current_path() + '\\samples\\expense_sample.xls'
         wb = open_workbook(filename=filename)
         ws = wb.sheet_by_name('Expense Report')
+
+        # find the row where the fields are
+        row = find_cell_string(ws, 0, 0, 'Value Date')
+        fields = read_expense_fields(ws, row)
+        fields_to_be = ['value_date', 'description', 'empty_field',
+                        'empty_field', 'amount', 'currency',
+                        'fx_rate', 'empty_field', 'hkd_equivalent']
+
+        self.assertEqual(len(fields), 9)
+        i = 0
+        for fld in fields:
+            self.assertEqual(fld, fields_to_be[i])
+            i = i + 1
+
+
+    def test_read_expense_item(self):
+        filename = get_current_path() + '\\samples\\expense_sample.xls'
+        wb = open_workbook(filename=filename)
+        ws = wb.sheet_by_name('Expense Report')
+
+        row = find_cell_string(ws, 0, 0, 'Value Date')
+        fields = read_expense_fields(ws, row)
+        expenses = []
+
+        row = 11    # read the first expense at A12
+        read_expense_item(ws, row, fields, expenses)
+        self.assertEqual(len(expenses), 1)
+        self.validate_expense_item(expenses[0])
+
+
+
+    def test_read_expense_item2(self):
+        filename = get_current_path() + '\\samples\\expense_sample.xls'
+        wb = open_workbook(filename=filename)
+        ws = wb.sheet_by_name('Expense Report')
+
+        row = find_cell_string(ws, 0, 0, 'Value Date')
+        fields = read_expense_fields(ws, row)
+        expenses = []
+
+        row = 13    # read the first expense at A14
+                    # no expense item will be read
+        read_expense_item(ws, row, fields, expenses)
+        self.assertEqual(len(expenses), 0)
+
+
+
+    def test_read_expense_item_error(self):
+        filename = get_current_path() + '\\samples\\expense_sample.xls'
+        wb = open_workbook(filename=filename)
+        ws = wb.sheet_by_name('Expense Report')
+
+        row = find_cell_string(ws, 0, 0, 'Value Date')
+        fields = read_expense_fields(ws, row)
+        expenses = []
+
+        row = 14    # read the expense at A15, suppose to generate error
+        with self.assertRaises(InvalidExpenseItem):
+            read_expense_item(ws, row, fields, expenses)
+        
+
+
+    def validate_expense_item(self, expense_item):
+        """
+        Validate the first expense item in expense_sample.xls
+        """
+        self.assertEqual(len(expense_item), 6)  # 6 data fields
+        self.assertEqual(expense_item['value_date'], datetime.datetime(2015,12,10))
+        self.assertEqual(expense_item['description'], 'Setup Fee')
+        self.assertAlmostEqual(expense_item['amount'], 602.12)
+        self.assertEqual(expense_item['currency'], 'HKD(港幣)')
+        self.assertEqual(expense_item['fx_rate'], 1.0)
+        self.assertAlmostEqual(expense_item['hkd_equivalent'], 602.12)
+
+
+
+
 
         
